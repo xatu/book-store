d6e1eda64e705d508c7bc63defc109843e5d4b97
xatu <rafaelf.mezquita@gmail.com>
2020-06-24 01:01:50 -0400

    Refactor

--- models/product.js
+++ models/product.js
@@ -2,32 +2,30 @@ const mongodb = require('mongodb')
 const { getDb } = require('../util/database')
 
 class Product {
-  constructor (title, price, description, imageUrl, id) {
+  constructor (title, price, description, imageUrl, id, userId) {
     this.title = title
     this.price = price
     this.description = description
     this.imageUrl = imageUrl
     this._id = id ? new mongodb.ObjectId(id) : null
+    this.userId = userId
   }
 
   save () {
     const db = getDb()
     let dbOp
     if (this._id) {
-      dbOp = db.collection('products').updateOne(
-        { _id: this._id },
-        { $set: this }
-      )
+      dbOp = db
+        .collection('products')
+        .updateOne({ _id: this._id }, { $set: this })
     } else {
       dbOp = db.collection('products').insertOne(this)
     }
     return dbOp
-      .then(result => {
+      .then((result) => {
         // console.log(result)
       })
-      .catch(
-        err => console.log(err)
-      )
+      .catch((err) => console.log(err))
   }
 
   static fetchAll () {
@@ -36,10 +34,10 @@ class Product {
       .collection('products')
       .find()
       .toArray()
-      .then(products => {
+      .then((products) => {
         return products
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err)
       })
   }
@@ -50,10 +48,10 @@ class Product {
       .collection('products')
       .find({ _id: new mongodb.ObjectId(productId) })
       .next()
-      .then(product => {
+      .then((product) => {
         return product
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err)
       })
   }
@@ -63,10 +61,10 @@ class Product {
     return db
       .collection('products')
       .deleteOne({ _id: new mongodb.ObjectId(productId) })
-      .then(resutl => {
+      .then((resutl) => {
         console.log('Deleted')
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error)
       })
   }
